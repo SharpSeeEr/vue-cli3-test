f48206a334123d82d4b3a502c1ae6aac727e2ee2
Aaron Throckmorton <aaron@throckmortons.com>
2018-04-04 13:01:12 -0600

    Add comment to Profile view, fix menu highlight

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,17 @@
 <template>
   <form>
     <h1>Profile</h1>
+    <!--
+      First Name, Last Name
+      Birtdate
+      Address - State should be a drop down
+      pHone
+      email
+      Subscribe?
+      Sex - radio buttons
+      Save button
+
+    -->
   </form>
 </template>
 
