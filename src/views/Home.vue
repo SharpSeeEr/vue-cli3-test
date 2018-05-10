2899821469407963afa730f0970bfdec67d7b171
Aaron Throckmorton <aaron@throckmortons.com>
2018-05-10 11:48:04 -0600

    Add timer functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,19 @@
     <img src="../assets/logo.png">
     <router-link to="profile">Profile</router-link>
     <time-tracker></time-tracker>
-    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld'
+import TimeTracker from '@/components/TimeTracker'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    TimeTracker
   }
 }
 </script>
