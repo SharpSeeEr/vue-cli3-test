d2427e17c42b9ddc3058d38b2e82e07148856dee
Aaron Throckmorton <aaron@throckmortons.com>
2018-06-20 10:41:39 -0600

    Store update

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+// @create-index
+
+export { default as HelloWorld } from './HelloWorld.vue';
+export { default as MInput } from './MInput.vue';
+
--- src/test.js
+++ src/test.js
@@ -1,1 +1,11 @@
 console.log('Hello World!')
+
+function createRange (length, start = 0) {
+  let result = [...Array(length + start).keys()].slice(start)
+  console.log(result.length, result)
+  return result
+}
+
+createRange(12)
+createRange(12, 1)
+createRange(12, 10)
